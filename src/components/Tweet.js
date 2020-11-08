c406dd88172d60a5ca23e81296bceb2af3ad8b53
Zaharia Ioan <zaharia.ioan@gmail.com>
2020-11-08 12:35:28 +0100

    styling using styled components

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,19 +1,27 @@
 import React from "react";
-import "../styles/Tweet.scss";
+import styled from "styled-components";
 
 const Tweet = ({ name, tweet, tweets, setTweets }) => {
   const deleteTweet = () => {
     setTweets(tweets.filter((state) => state.id !== tweet.id));
   };
   return (
-    <div className="tweet">
+    <TweetStyle>
       <h2>{name}</h2>
       <h3>{tweet.message}</h3>
       <button onClick={deleteTweet}>Delete</button>
       <button>Like</button>
-    </div>
+    </TweetStyle>
   );
 };
+
+const TweetStyle = styled.div`
+  background-color: "red";
+  border: 2px solid red;
+  border-radius: 25px;
+  padding: 10px;
+`;
+
 // const Tweet = (props) => {
 //   return (
 //     <div className="tweet">
